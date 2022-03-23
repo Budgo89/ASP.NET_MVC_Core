6ad88d5b416717f75b4754a6a609e835ba3a90b4
Maks Sapunov <Budgo@mail.ru>
2022-03-23 22:09:07 +0300

    Задание 2

--- ASP.NET MVC Core/ASP.NET MVC Core/Form1.Designer.cs
+++ ASP.NET MVC Core/ASP.NET MVC Core/Form1.Designer.cs
@@ -29,7 +29,8 @@
         private void InitializeComponent()
         {
             this.button_start = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox_fibonacci = new System.Windows.Forms.TextBox();
+            this.textBox_time = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button_start
@@ -42,19 +43,27 @@
             this.button_start.UseVisualStyleBackColor = true;
             this.button_start.Click += new System.EventHandler(this.button_start_Click);
             // 
-            // textBox1
+            // textBox_fibonacci
             // 
-            this.textBox1.Location = new System.Drawing.Point(74, 83);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(646, 23);
-            this.textBox1.TabIndex = 3;
+            this.textBox_fibonacci.Location = new System.Drawing.Point(74, 87);
+            this.textBox_fibonacci.Name = "textBox_fibonacci";
+            this.textBox_fibonacci.Size = new System.Drawing.Size(541, 23);
+            this.textBox_fibonacci.TabIndex = 3;
+            // 
+            // textBox_time
+            // 
+            this.textBox_time.Location = new System.Drawing.Point(637, 87);
+            this.textBox_time.Name = "textBox_time";
+            this.textBox_time.Size = new System.Drawing.Size(129, 23);
+            this.textBox_time.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox_time);
+            this.Controls.Add(this.textBox_fibonacci);
             this.Controls.Add(this.button_start);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -65,6 +74,7 @@
 
         #endregion
         private Button button_start;
-        private TextBox textBox1;
+        private TextBox textBox_fibonacci;
+        private TextBox textBox_time;
     }
 }
--- ASP.NET MVC Core/ASP.NET MVC Core/Form1.cs
+++ ASP.NET MVC Core/ASP.NET MVC Core/Form1.cs
@@ -1,7 +1,10 @@
+using System.Globalization;
+
 namespace ASP.NET_MVC_Core
 {
     public partial class Form1 : Form
     {
+        const double sec = 1000;
         public Form1()
         {
             InitializeComponent();
@@ -10,34 +13,60 @@ namespace ASP.NET_MVC_Core
         private int num0 = 0;
         private int num1 = 1;
         private int num2 = 0;
-        public void Fibonacci()
+        public void Fibonacci(int sleep)
         {
-            int sleep = 2000;
+           ;
             num0 = num0 + num1;
-            textBox1.BeginInvoke(new Action(() =>
+            textBox_fibonacci.BeginInvoke(new Action(() =>
             {
-                textBox1.Text = num0.ToString();
+                textBox_fibonacci.Text = num0.ToString();
             }));
             Thread.Sleep(sleep);
             while (true)
             {
                 num2 = num0 + num1;
-                textBox1.BeginInvoke(new Action(() =>
+                textBox_fibonacci.BeginInvoke(new Action(() =>
                 {
-                    textBox1.Text = num0.ToString();
+                    textBox_fibonacci.Text = num0.ToString();
                 }));
                 
                 num0 = num1;
                 num1 = num2;
+                var timer = new Thread(() =>
+                {
+                    Timer(sleep);
+                });
+                timer.Start();
                 Thread.Sleep(sleep);
             }
             // ReSharper disable once FunctionNeverReturns
         }
 
+        public void Timer(int timer)
+        {
+            int sleep = 200;
+            var specifier = "G";
+            var culture = CultureInfo.CreateSpecificCulture("eu-ES");
+            
+            for (double i = timer; i >= 0; )
+            {
+                textBox_time.BeginInvoke(new Action(() =>
+                {
+                    if (i >= 0)
+                        textBox_time.Text = (i / sec).ToString(specifier, culture);
+                }));
+                i -= sleep;
+                Thread.Sleep(sleep);
+            }
+        }
         private void button_start_Click(object sender, EventArgs e)
         {
-            Thread fibonacci = new Thread(Fibonacci);
-            fibonacci.Start();
+            var sleep = 2000;
+            new Thread(() =>
+            {
+                Fibonacci(sleep);
+            }).Start();
+            
             button_start.Visible = false;
         }
     }
