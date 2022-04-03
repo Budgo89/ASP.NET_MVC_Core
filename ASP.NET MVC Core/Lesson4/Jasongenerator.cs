7f934b7f0719438dffcb25c72e53099ab007a2e3
LAPTOP-PSEPRFOJ\Максим <budgo@mail.ru>
2022-04-03 16:26:28 +0300

    Рефакторинг

---
+++ ASP.NET MVC Core/Lesson4/Jasongenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Lesson4.Models;
+
+namespace Lesson4
+{
+    public class Jasongenerator
+    {
+        public Jasongenerator()
+        {
+            List<Phone> phones = new List<Phone>();
+            List<Auto> autos = new List<Auto>();
+            List<Sticks> sticks = new List<Sticks>();
+
+            phones.Add(new Phone("iPhone", "13 Pro", 150000));
+            phones.Add(new Phone("HONOR", "50 Lite", 100000));
+            phones.Add(new Phone("OnePlus", "7 Pro", 100000));
+
+            var file = new FileInfo("Phone.json");
+
+            using (var json = file.Create())
+            {
+                JsonSerializer.Serialize(json, phones);
+            }
+
+            autos.Add(new Auto("AvtoVAZ ", "Vesta", 1000000));
+            autos.Add(new Auto("Hyundai ", "Solaris", 1500000));
+            autos.Add(new Auto("Mercedes", "EQE", 15000000));
+            file = new FileInfo("Auto.json");
+
+            using (var json = file.Create())
+            {
+                JsonSerializer.Serialize(json, autos);
+            }
+
+            sticks.Add(new Sticks("HEETS", "Purple", 160));
+            sticks.Add(new Sticks("Fiit ", "Tropic", 1500000));
+            file = new FileInfo("Sticks.json");
+
+            using (var json = file.Create())
+            {
+                JsonSerializer.Serialize(json, sticks);
+            }
+        }
+    }
+}
--- ASP.NET MVC Core/Lesson4/Program.cs
+++ ASP.NET MVC Core/Lesson4/Program.cs
@@ -1,38 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Text.Json;
+using Lesson4;
 using Lesson4.Models;
 
-List<Phone> phones = new List<Phone>();
-List<Auto> autos = new List<Auto>();
-List<Sticks> sticks = new List<Sticks>();
+#region генератор
 
-phones.Add(new Phone("iPhone", "13 Pro", 150000));
-phones.Add(new Phone("HONOR", "50 Lite", 100000));
-phones.Add(new Phone("OnePlus", "7 Pro", 100000));
+//_ = new Jasongenerator();
 
-var file = new FileInfo("Phone.json");
+#endregion
 
-using (var json = file.Create())
-{
-    JsonSerializer.Serialize(json, phones);
-}
-
-autos.Add(new Auto("AvtoVAZ ", "Vesta", 1000000));
-autos.Add(new Auto("Hyundai ", "Solaris", 1500000));
-autos.Add(new Auto("Mercedes", "EQE", 15000000));
-file = new FileInfo("Auto.json");
-
-using (var json = file.Create())
-{
-    JsonSerializer.Serialize(json, autos);
-}
-
-sticks.Add(new Sticks("HEETS", "Purple", 160));
-sticks.Add(new Sticks("Fiit ", "Tropic", 1500000));
-file = new FileInfo("Sticks.json");
-
-using (var json = file.Create())
-{
-    JsonSerializer.Serialize(json, sticks);
-}
